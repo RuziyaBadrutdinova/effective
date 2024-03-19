6e06e68be00e9311ad50c6a28b3575ee298fb523
RuziyaBadrutdinova <40975953+RuziyaBadrutdinova@users.noreply.github.com>
2024-03-19 09:34:24 +0300

    add files

---
+++ src/main/java/ru/task/Test1.java
@@ -0,0 +1,18 @@
+package ru.task;
+
+/**
+ * @author RuziyaBadrutdinova
+ * @since 17.03.2024
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(new StringConverter().convertObjects("-",0, 1));
+        System.out.println(new StringConverter().convertObjects("-",7L));
+        System.out.println(new StringConverter().convertObjects("/",false, true));
+        System.out.println(new StringConverter().convertObjects(";","q", "er", "uy", "h g"));
+        System.out.println(new StringConverter().convertObjects(";","x", "c", "", "k"));
+        System.out.println(new StringConverter().convertObjects("<>", "adg", "kkj", "5", 8, 4));
+        System.out.println(new StringConverter().convertObjects("<>", null, "kkj", "5"));
+    }
+
+}
